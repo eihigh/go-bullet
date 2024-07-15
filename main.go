92425bb5bde15b4a2b5a5d9ad834838a2f201207
eihigh <eihigh.contact@gmail.com>
2024-07-15 17:55:45 +0900

    add comments

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 // 事実上のメイン関数
 func top(next next) {
 	for range 120 {
-		fireArrowBullet(vw/2, vh/13, tau/4)
-		next.skip(30)
+		fireArrowBullet(vw/2, vh/13, tau/4) // 画面中央上辺りから、下に向けて矢印弾幕を発射
+		next.skip(30)                       // 30フレーム待つ
 	}
 }
